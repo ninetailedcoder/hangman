4f494eb32321658337b2623804d21d5f53675ff7
ninetailedcoder <torydemaio@gmail.com>
2022-08-16 15:26:06 -0500

    Hangman main file and random words file

---
+++ words.py
@@ -0,0 +1,8 @@
+words=['tesla','lucid','bmw','dodge',"bring","according","avoid","dot","forest","tightly"
+"snow","play","large","vertical","advice","softly"
+"union","sunlight","duty","driven","clay","saddle"
+"truth","caught","wide","warn","sang","path"
+"fifth","pig","slave","division","handle","pale"
+"trunk","fact","stream","empty","member","respect"
+"sheet","hurry","hit","century","voyage","down"]
+
